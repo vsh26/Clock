72d0e6478731b54729330f3401c234f001b3f411
Vishal Kapgate <vishaldk26@gmail.com>
2025-03-20 08:50:36 +0530

    Implement Time Format selector

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const selectElement = document.getElementById("time-format-picker");
+
+let selectedValue = 'default';
+
+
 function updateClock(){
 
     const timeElement = document.getElementById("time-container");
@@ -6,15 +11,26 @@ function updateClock(){
     const now = new Date();
     // console.log(now);
 
-    const hours = (now.getHours() % 12 || 12).toString().padStart(2, "0");
-    // const minutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
-    // const seconds = now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds();
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
+    if(selectedValue === 'am-pm'){
 
-    const ampm = (now.getHours() >= 12 ? "PM" : "AM");
+        const hours = (now.getHours() % 12 || 12).toString().padStart(2, "0");
+        // const minutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+        // const seconds = now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds();
+        const minutes = now.getMinutes().toString().padStart(2, "0");
+        const seconds = now.getSeconds().toString().padStart(2, "0");
+    
+        const ampm = (now.getHours() >= 12 ? "PM" : "AM");
+    
+        timeElement.textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
 
-    timeElement.textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
+    }else{
+
+        const hours = (now.getHours()).toString().padStart(2, "0");
+        const minutes = now.getMinutes().toString().padStart(2, "0");
+        const seconds = now.getSeconds().toString().padStart(2, "0");
+
+        timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+    }
 
     const options = {
         weekday: "long",
@@ -26,6 +42,15 @@ function updateClock(){
     // undefined - according to local timezone and default locale
     dateElement.textContent = now.toLocaleDateString(undefined, options);
 }
+
+selectElement.addEventListener('click', () => {
+
+    selectedValue = selectElement.value;
+    // console.log(selectedValue);
+
+    updateClock();
+    
+});
 
 setInterval(updateClock, 1000);
 updateClock();
